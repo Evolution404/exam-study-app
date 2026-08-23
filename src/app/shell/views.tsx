fb6b9c0a928afb5e19965d1402b719cb64df609c
Evolution404 <35091674+Evolution404@users.noreply.github.com>
2026-08-23 15:16:12 +0800

    refactor: narrow shell view barrel exports

--- src/app/shell/views.tsx
+++ src/app/shell/views.tsx
@@ -2,17 +2,5 @@
 
 export { PullToRefresh } from "./views/pull-to-refresh";
 export { Dashboard } from "./views/dashboard";
-export { Stat } from "./views/stat";
-export { EmptyImport } from "./views/empty-import";
 export { PreferencesView } from "./views/preferences-view";
-export { ImageCacheSetting } from "./views/image-cache-setting";
-export { SyncAutomationSetting } from "./views/sync-automation-setting";
-export { NumberPreference } from "./views/number-preference";
-export { ThemeSetting } from "./views/theme-setting";
-export { PreferenceSelect } from "./views/preference-select";
-export { GoalSetting } from "./views/goal-setting";
-export { GroupSizeSetting } from "./views/group-size-setting";
-export { ToleranceSetting } from "./views/tolerance-setting";
-export { BuildVersionCard } from "./views/build-version-card";
 export { Practice } from "./views/practice";
-export { QuestionOverview } from "./views/question-overview";
